f4662f2f3479321c9822df6e3c0dda967f09cfdf
Nash <141100397+s2an@users.noreply.github.com>
2024-06-03 15:23:05 -0400

    feat: Add search_projects & refactor: Pull connection out of response

--- app/services/oshwa_service.rb
+++ app/services/oshwa_service.rb
@@ -3,15 +3,35 @@ class OshwaService
   BASE_URL = "https://certificationapi.oshwa.org"
 
   def self.get_projects
-    response = Faraday.get("#{BASE_URL}/api/projects") do |req|
+    response = conn.get("#{BASE_URL}/api/projects") do |req|
       req.headers["Content-Type"] = "application/json"
-      req.body = { bearer_token: ENV["oshwa_bearer_token"] }.to_json
+      req.headers["Authorization"] = "Bearer #{ENV["oshwa_bearer_token"]}"
     end
 
     if response.success?
       JSON.parse(response.body)
     else
       raise "Error: #{response.status} - #{response.reason_phrase}"
+    end
+  end
+
+  def self.search_projects(q)
+    response = conn.get("/api/projects?q=#{q}") do |req|
+      req.headers["Content-Type"] = "application/json"
+      req.headers["Authorization"] = "Bearer #{ENV["oshwa_bearer_token"]}"
+    end
+
+    if response.success?
+      JSON.parse(response.body)
+    else
+      raise "Error: #{response.status} - #{response.reason_phrase}"
+    end
+  end
+
+  def self.conn #5
+    Faraday.new(url: BASE_URL) do |faraday|
+      faraday.request :url_encoded
+      faraday.adapter Faraday.default_adapter
     end
   end
 end
