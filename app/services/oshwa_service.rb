4cee2e46c309db48b43e72629b742ece627b73b7
Nash <141100397+s2an@users.noreply.github.com>
2024-06-04 11:47:17 -0400

    refactor: DRY oshwa service

--- app/services/oshwa_service.rb
+++ app/services/oshwa_service.rb
@@ -3,16 +3,12 @@ class OshwaService
   BASE_URL = "https://certificationapi.oshwa.org"
 
   def self.get_projects
-    response = conn.get("#{BASE_URL}/api/projects") do |req|
+    response = conn.get("/api/projects") do |req|
       req.headers["Content-Type"] = "application/json"
       req.headers["Authorization"] = "Bearer #{ENV["oshwa_bearer_token"]}"
     end
 
-    if response.success?
-      JSON.parse(response.body)
-    else
-      raise "Error: #{response.status} - #{response.reason_phrase}"
-    end
+    parse_response(response)
   end
 
   def self.search_projects(q)
@@ -21,17 +17,23 @@ class OshwaService
       req.headers["Authorization"] = "Bearer #{ENV["oshwa_bearer_token"]}"
     end
 
-    if response.success?
-      JSON.parse(response.body)
-    else
-      raise "Error: #{response.status} - #{response.reason_phrase}"
-    end
+    parse_response(response)
   end
 
   def self.conn #5
     Faraday.new(url: BASE_URL) do |faraday|
       faraday.request :url_encoded
       faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  private
+
+  def self.parse_response(response)
+    if response.success?
+      JSON.parse(response.body)
+    else
+      raise "Error: #{response.status} - #{response.reason_phrase}"
     end
   end
 end
